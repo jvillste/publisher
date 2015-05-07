b4cbf1500c4f39e75db9f17ca6a5a6f00e79d054
Jukka Villstedt <jukka.villstedt@gmail.com>
2015-05-08 01:26:09 +0300

    added disqus

--- src/publisher/core.clj
+++ src/publisher/core.clj
@@ -6,6 +6,7 @@
             [hiccup.core :as hiccup]
             [clojure.string :as string])
   (:import [java.io File]
+           [java.util Calendar Date]
            [java.net URLEncoder URLDecoder]))
 
 (defn files-in-directory [directory-path]
@@ -17,11 +18,44 @@
 
 (def directory "/Users/jukka/Downloads/kuntotiedot/68356_kiinteistojen_kuntotiedot_Vaahterakuja-Vaskivuori")
 
+(def facebook-sdk [[:div {:id "fb-root"}]
+                   [:script "(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = \"//connect.facebook.net/fi_FI/sdk.js#xfbml=1&version=v2.3\";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));"]])
+
+(def like-button [:div {:class "fb-like"
+                        :data-href="https://developers.facebook.com/docs/plugins/"
+                        :data-layout="standard"
+                        :data-action="like"
+                        :data-show-faces="true"
+                        :data-share="true"}])
+
+(def disqus "<div id=\"disqus_thread\"></div>
+  <script type=\"text/javascript\">
+  /* * * CONFIGURATION VARIABLES * * */
+  var disqus_shortname = 'kuntotiedot';
+  
+  /* * * DON'T EDIT BELOW THIS LINE * * */
+  (function() {
+  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+  </script>
+<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\" rel=\"nofollow\">comments powered by Disqus.</a></noscript>")
+
 (defn page [& content]
   (hiccup/html [:html
                 [:head
                  [:title "Kuntotiedot"]]
-                [:body content]]))
+                [:body (concat facebook-sdk
+                               content)]]))
+
+
 
 (defn fix-file-name [file-name]
   (-> file-name
@@ -30,6 +64,24 @@
 (defn file-name [file]
   (-> (.getName file)
       (fix-file-name)))
+
+(defn date-to-map [date]
+  (let [calendar (Calendar/getInstance)]
+    (.setTime calendar date)
+    {:year (.get calendar Calendar/YEAR)
+     :month (+ 1 (.get calendar Calendar/MONTH))
+     :day (.get calendar Calendar/DAY_OF_MONTH)
+     :hour (.get calendar Calendar/HOUR_OF_DAY)
+     :minute (.get calendar Calendar/MINUTE)
+     :second (.get calendar Calendar/SECOND)}))
+
+(defn date-string [{:keys [year month day]}]
+  (str day "." month "." year))
+
+
+
+(defn last-modified-date [file]
+  (date-to-map (Date. (.lastModified file))))
 
 (defn app []
   (compojure/routes (compojure/GET "/" [] (page [:ul (for [file (.listFiles (File. directory))]
@@ -47,7 +99,14 @@
                     (compojure/GET "/:folder/:file" [folder file] (page [:a {:href (str "/get/" folder "/" file)}
                                                                          (-> file
                                                                              (URLDecoder/decode)
-                                                                             (fix-file-name))]))))
+                                                                             (fix-file-name))
+                                                                         " "
+                                                                         (-> (str directory "/" (URLDecoder/decode folder) "/" (URLDecoder/decode file))
+                                                                             (File.)
+                                                                             (last-modified-date)
+                                                                             (date-string))]
+                                                                        like-button
+                                                                        disqus))))
 
 (def handler (app))
 
